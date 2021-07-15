0613d7b312842b95143bb8d96709b33a19521ad4
rupali23-singh <69974338+rupali23-singh@users.noreply.github.com>
2021-07-15 22:28:02 +0530

    Add files via upload

---
+++ first_question.py
@@ -0,0 +1,15 @@
+
+my_file2 = open("people2.txt", "w")
+my_file2.write("Abhishek - Gurgaon")
+my_file2.write("Ranveer - Delhi")
+my_file2.close()
+
+
+
+
+
+
+# my_file = open("people1.txt")
+# file_data = my_file.read()
+# print (file_data)
+# my_file.close() 
